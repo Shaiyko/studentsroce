bb15b61d9316efec4b5fd72424754ff044b395f8
Shaiyko <dexprojectkk@gmail.com>
2025-09-02 14:39:45 +0700

    seve 2/09/2025

--- src/URL.jsx
+++ src/URL.jsx
@@ -1,11 +1,13 @@
 const apiss = "https://apisroce.onrender.com";
 const apigo = "https://apisroceshv.onrender.com";
+const apirailway = "https://apisroce-production.up.railway.app";
 const apisl = "http://localhost:4005";
 
 const apisheets = "https://sheetapi-vw0g.onrender.com";
 const apisheetgo = "https://sheetapigo.onrender.com";
+const apisheetrailway = "https://sheetapi-production-0624.up.railway.app";
 const apisheetlocal = "http://localhost:4002";
 
-export const apisroot = apigo;
-export const apisheet = apisheetgo;
+export const apisroot = apirailway;
+export const apisheet = apisheetrailway;
 export const apiemail = "https://gmail-api-pefn.onrender.com";
